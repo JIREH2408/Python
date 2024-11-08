ed7343f87d28e7cbe091b7b8ff8875704c8b0c24
jireharango <jireharango@gmail.com>
2024-11-08 17:20:24 -0500

    condicionales

--- condicionales2.py
+++ condicionales2.py
@@ -3,10 +3,8 @@ Nombre = (input("Nombre del estudiante: "))
 N1 = float(input("Ingresar la nota numero 1: "))
 N2 = float(input("ingresar la nota numero 2: "))
 N3 = float(input("ingresar la nota numero 3: "))
-N4 = float(input("ingresar la nota numero 4: "))
-N5 = float(input("ingresar la nota numero 5: "))
 
-promedio = (N1 + N2 + N3 + N4 + N5) /5
+promedio = (N1 + N2 + N3) /3
 print ("Nombre", Nombre)
 print ("La nota final del estudiante es: ", round (promedio,1))
 
